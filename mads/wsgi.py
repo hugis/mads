db576f5a05045ae9f48a69fec121eec82809a5f6
Peter Hyben <peter.hyben@tag.sk>
2019-07-03 18:01:57 +0200

    Project inicialization

---
+++ mads/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mads project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mads.settings")
+
+application = get_wsgi_application()
